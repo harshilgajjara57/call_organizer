ee1f24bd4ab11474de58d933e72b67d2022ec50a
Harshil Gajjar <harshilgajjara57@gmail.com>
2025-04-04 01:47:30 +0530

    change permission handler file location

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'permission_handler_manager.dart';
+import 'permission_handler/permission_handler_manager.dart';
 
 void main() {
   runApp(const MyApp());
